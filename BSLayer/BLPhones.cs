60873afa96ca543832a079e00a763e0ee42176d4
waflol <lenguyenhunganh2001@gmail.com>
2021-06-19 13:02:44 +0700

    HA

--- BSLayer/BLPhones.cs
+++ BSLayer/BLPhones.cs
@@ -4,9 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace PhoneManagerment_ADO.net.BSLayer
 {
     class BLPhones
     {
+        DBMain db = null;
     }
 }
