d236f42ad97346270871caa83915949461ea0a7c
Thế Hiển <huynhthehien11102001@gmail.com>
2021-06-19 18:46:09 +0700

    Hien ham addphone

--- Form_AddNewPhone.cs
+++ Form_AddNewPhone.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using PhoneManagerment_ADO.net.BSLayer;
 namespace PhoneManagerment_ADO.net
 {
     public partial class Form_AddNewPhone : Form
@@ -16,6 +16,11 @@ namespace PhoneManagerment_ADO.net
         {
             InitializeComponent();
         }
-
+       
+        private void btn_Save_Click(object sender, EventArgs e)
+        {
+            BLPhones phone = new BLPhones();
+            phone.addPhones(Convert.ToInt32(txt_IDphone.Text), txt_ModelName.Text, cbb_Ram.Text, cbb_FrontCamera.Text, cbb_SimType.Text, cbb_NetworkType.Text, txt_Price.Text, cbb_FingerprintSensor.Text, cbb_InternalStorage.Text, cbb_InternalStorage.Text, cbb_RearCamera.Text);
+        }
     }
 }
