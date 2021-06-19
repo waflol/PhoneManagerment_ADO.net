7f4e0ecb4221eb023ec0b0db18f61e7a417e740b
waflol <lenguyenhunganh2001@gmail.com>
2021-06-19 22:28:30 +0700

    HA

--- BSLayer/BLTransaction_details.cs
+++ BSLayer/BLTransaction_details.cs
@@ -18,7 +18,20 @@ namespace PhoneManagerment_ADO.net.BSLayer
         }
         public DataSet getTransaction_details()
         {
-            string sqlString = "select * from "
+            string sqlString = "select * from Customer, Phone, Transaction_Details, Transactions where Customer.ID_customer = Transactions.ID_customer and Transactions.ID_transaction = Transaction_Details.ID_transaction and Transaction_Details.ID_phone = Phone.ID_phone";
+            return db.ExcuteQueryDataSet(sqlString, CommandType.Text);
+        }
+
+        public bool addTransaction_details(string quantity, string idtransaction, string idphone)
+        {
+            string sqlString = "Insert Into Transaction_Details Values(" + quantity + "," + idtransaction + "," + idphone + ")";
+            return db.MyExecuteNonQuery(sqlString, CommandType.Text, ref err);
+        }
+
+        public bool deleteTransaction_details(string idtransaction)
+        {
+            string sqlString = "Delete From Phone where ID_phone = " + idtransaction;
+            return db.MyExecuteNonQuery(sqlString, CommandType.Text, ref err);
         }
 
     }
