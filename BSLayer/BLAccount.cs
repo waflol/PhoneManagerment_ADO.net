f39d11a5cb8d3a76a8a921ef6d410b1c8d413db6
waflol <lenguyenhunganh2001@gmail.com>
2021-06-19 20:00:47 +0700

    HA

--- BSLayer/BLAccount.cs
+++ BSLayer/BLAccount.cs
@@ -35,13 +35,25 @@ namespace PhoneManagerment_ADO.net.BSLayer
             else return true;
         }
 
+        public bool ExistAccount(string user)
+        {
+            string sqlcmm = "select * from Accounts where username = '" + user + "'";
+
+            DataSet ds = db.ExcuteQueryDataSet(sqlcmm, CommandType.Text);
+
+            if (ds == null || ds.Tables.Count == 0 || ds.Tables[0].Rows.Count == 0)
+            {
+                return false;
+            }
+            else return true;
+        }
         // them phone
         public bool addAccount(string user, string pass)
         {
             return false;
         }
         //xoa phone
-        public bool deletePhones(ref string err, string user)
+        public bool deleteAccount(ref string err, string user)
         {
             return false;
         }
