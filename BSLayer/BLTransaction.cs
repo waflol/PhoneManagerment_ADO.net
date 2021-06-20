dd7f75f7755301a2a180e9a9d6fc6fdc6c7d91a6
waflol <lenguyenhunganh2001@gmail.com>
2021-06-20 12:23:52 +0700

    HA

--- BSLayer/BLTransaction.cs
+++ BSLayer/BLTransaction.cs
@@ -42,7 +42,8 @@ namespace PhoneManagerment_ADO.net.BSLayer
         // them phone
         public bool addTransaction(int idtrans, int totalprice, string date, int idcus, string username)
         {
-            string sqlString = "Insert Into Transactions Values("+idtrans.ToString()+","+totalprice.ToString()+",'"+date+"',"+idcus.ToString()+",'"+username+"')";
+            //string sqlString = "Insert Into Transactions Values("+idtrans.ToString()+","+totalprice.ToString()+",'"+date+"',"+idcus.ToString()+",'"+username+"')";
+            string sqlString = "Insert into Transactions Values("+idtrans.ToString()+","+totalprice.ToString()+",CONVERT(datetime,'"+date+"',103),"+idcus.ToString()+",'"+ username +"')";
             return db.MyExecuteNonQuery(sqlString, CommandType.Text, ref err);
         }
         //xoa phone
